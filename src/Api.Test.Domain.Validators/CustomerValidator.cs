ca913c0af5d8d5734a81aa7d4ca3555e79ee3c64
Robert Szachnowski <rafeq1241@gmail.com>
2021-08-15 22:10:47 +0200

    Introduce Validators project for better granulation. Add common CustomerValidator implementation.

---
+++ src/Api.Test.Domain.Validators/CustomerValidator.cs
@@ -0,0 +1,36 @@
+﻿using Api.Test.Domain.Models;
+using FluentValidation;
+
+namespace Api.Test.Domain.Validators
+{
+    public abstract class CustomerValidator : AbstractValidator<Customer>
+    {
+        protected CustomerValidator()
+        {
+            SetRules();
+        }
+
+        private void SetRules()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+            RuleFor(x => x.LastName)
+                .NotEmpty();
+            RuleFor(x => x.Address)
+                .SetValidator(new AddressValidator());
+        }
+    }
+
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(x => x.Number)
+                .NotEmpty();
+            RuleFor(x => x.Street)
+                .NotEmpty();
+            RuleFor(x => x.ZipCode)
+                .NotEmpty();
+        }
+    }
+}
