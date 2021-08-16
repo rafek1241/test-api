972e594364a25432c71fa174d994bb5464fbc583
Robert Szachnowski <rafeq1241@gmail.com>
2021-08-16 20:04:13 +0200

    Log error of unexpected exceptions.

--- src/Api.Test.API/Filters/GeneralExceptionFilter.cs
+++ src/Api.Test.API/Filters/GeneralExceptionFilter.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
 
 namespace Api.Test.API.Filters
 {
     public class GeneralExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger _logger;
+
+        public GeneralExceptionFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
         public void OnException(ExceptionContext context)
         {
+            _logger.Error(context.Exception, context.Exception.Message);
             context.Result = new ObjectResult("Please contact with administrator.")
             {
                 StatusCode = StatusCodes.Status500InternalServerError
