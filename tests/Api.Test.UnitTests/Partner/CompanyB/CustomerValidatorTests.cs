c5b1215b091c8b79ffa4c491ee973df3eb285d5e
Robert Szachnowski <rafeq1241@gmail.com>
2021-08-16 20:44:57 +0200

    Unit test for CompanyB's customer validation.

--- tests/Api.Test.UnitTests/Partner/CompanyB/CustomerValidatorTests.cs
+++ tests/Api.Test.UnitTests/Partner/CompanyB/CustomerValidatorTests.cs
@@ -1,20 +1,52 @@
 using System;
+using System.Threading.Tasks;
+using Api.Test.CompanyB;
+using Api.Test.Domain.Models;
+using AutoFixture;
+using FluentAssertions;
+using FluentValidation.TestHelper;
 using Xunit;
 
 namespace Api.Test.UnitTests.Partner.CompanyB
 {
     public class CustomerValidatorTests
     {
-        [Fact]
-        public void when_favorite_football_team_missing__returns_invalid()
+        private readonly IFixture _fixture = FixtureFactory.Instance;
+        private CustomerValidator _validator;
+
+        public CustomerValidatorTests()
         {
-            throw new NotImplementedException();
+            _validator = _fixture.Create<CustomerValidator>();
+        }
+
+        
+        [Fact]
+        public async Task when_favorite_football_team_missing__returns_invalid()
+        {
+            var instance = _fixture.Build<Customer>()
+                .Without(x => x.FavoriteFootballTeam)
+                .Create();
+
+            var result = await _validator.TestValidateAsync(instance);
+            
+            instance.FavoriteFootballTeam
+                .Should()
+                .BeNullOrEmpty();
+            
+            result.ShouldHaveValidationErrorFor(x => x.FavoriteFootballTeam);
+            result.IsValid.Should().BeFalse();
         }
 
         [Fact]
-        public void when_favorite_football_team_filled__returns_valid()
+        public async Task when_favorite_football_team_filled__returns_valid()
         {
-            throw new NotImplementedException();
+            var instance = _fixture.Create<Customer>();
+            
+            var result = await _validator.TestValidateAsync(instance);
+            
+            instance.FavoriteFootballTeam.Should().NotBeEmpty();
+            result.ShouldNotHaveValidationErrorFor(x => x.FavoriteFootballTeam);
+            result.IsValid.Should().BeTrue();
         }
     }
 }
